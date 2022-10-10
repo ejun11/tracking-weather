db65b9f6c381fb37a27e2f19e2af1202d84f9927
ejun11 <114144254+ejun11@users.noreply.github.com>
2022-10-10 21:59:39 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
-type weatherData struct {
+type WeatherData struct {
 	Name  string `json:"name`
 	Id    int    `json:"id`
 	Coord struct {
@@ -25,22 +26,37 @@ type weatherData struct {
 		Weather     string `json:"main"`
 		Description string `json:"description"`
 	} `json:"weather"`
+
+	Datetime int64     `json:"dt"`
+	Timezone int64     `json:"timezone"`
+	DTime    time.Time `json:"ConvertUnix"`
 }
 
-func query(city string) (weatherData, error) {
+func query(city string) (WeatherData, error) {
 
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=8bd90625c19d08d8d1a1c20bd04b9ce0" + "&q=" + city + "&units=metric")
 	if err != nil {
-		return weatherData{}, err
+		return WeatherData{}, err
 	}
 
 	defer resp.Body.Close()
 
-	var d weatherData
+	var d WeatherData
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
+		return WeatherData{}, err
 	}
+	converttoDate(d)
 	return d, nil
+}
+
+func converttoDate(d WeatherData) {
+	x := d.Datetime
+	now := time.Now()
+	ConvertUnix := time.Unix(x, 0)
+
+	fmt.Println("\nCity:", d.Name)
+	fmt.Println("Convert GMT:", ConvertUnix)
+	fmt.Println("Local Time:", now)
 }
 
 func main() {
