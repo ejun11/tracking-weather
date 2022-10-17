57b126e7ca0c8d4e982c384125188b5315a4784a
ejun11 <114144254+ejun11@users.noreply.github.com>
2022-10-18 00:03:22 +0700

    Update main.go

--- main.go
+++ main.go
@@ -27,9 +27,12 @@ type WeatherData struct {
 		Description string `json:"description"`
 	} `json:"weather"`
 
-	Datetime int64     `json:"dt"`
-	Timezone int64     `json:"timezone"`
-	DTime    time.Time `json:"ConvertUnix"`
+	Datetime int64 `json:"dt"`
+	Timezone int64 `json:"timezone"`
+}
+
+type Timelocal struct {
+	Name string `json:"name`
 }
 
 func query(city string) (WeatherData, error) {
@@ -45,21 +48,42 @@ func query(city string) (WeatherData, error) {
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
 		return WeatherData{}, err
 	}
-	converttoDate(d)
 	return d, nil
 }
 
-func converttoDate(d WeatherData) {
-	x := d.Datetime
-	now := time.Now()
-	ConvertUnix := time.Unix(x, 0)
+func timelocal(city string) (Timelocal, error) {
 
-	fmt.Println("\nCity:", d.Name)
-	fmt.Println("Convert GMT:", ConvertUnix)
-	fmt.Println("Local Time:", now)
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=8bd90625c19d08d8d1a1c20bd04b9ce0" + "&q=" + city + "&units=metric")
+	if err != nil {
+		return Timelocal{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var e Timelocal
+	if err := json.NewDecoder(resp.Body).Decode(&e); err != nil {
+		return Timelocal{}, err
+	}
+	return e, nil
 }
 
 func main() {
+
+	http.HandleFunc("/waktu/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := timelocal(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			now := time.Now().Local()
+			tf := now.Format("2006-1-2 15:04:05")
+			fmt.Println("GMT:", tf)
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+			json.NewEncoder(w).Encode("GMT: " + tf)
+		})
 
 	http.HandleFunc("/tracking-weather/",
 		func(w http.ResponseWriter, r *http.Request) {
